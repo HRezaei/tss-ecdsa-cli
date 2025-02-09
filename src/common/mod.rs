7c8ce8e63d7e37e6537de59aa7ff277ed8a39a2c
hrezaei <mr.hrezaei@gmail.com>
2025-02-09 12:25:14 +0000

    Update crate rand from 0.8.5 to 0.9.0

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -16,7 +16,7 @@ use multi_party_ecdsa::protocols::multi_party_ecdsa::gg_2018::party_i::{Keys, Sh
 use paillier::EncryptionKey;
 use reqwest::blocking::Client as RequestClient;
 use serde::{Deserialize, Serialize};
-use rand::{rngs::OsRng, RngCore};
+use rand::{rngs::OsRng, TryRngCore};
 use sha2::{Sha256, Digest};
 
 
@@ -106,7 +106,7 @@ pub fn aes_encrypt(key: &[u8], plaintext: &[u8]) -> AEAD {
     let cipher = Aes256Gcm::new(aes_key);
 
     let mut nonce = [0u8; 12];
-    OsRng.fill_bytes(&mut nonce);
+    let _ = OsRng.try_fill_bytes(&mut nonce);
     let nonce = Nonce::from_slice(&nonce);
 
     let ciphertext = cipher
